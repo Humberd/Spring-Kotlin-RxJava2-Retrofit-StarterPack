35ad0469b333265edb61d14bc684f459676007db
Humberd <sawik11@wp.pl>
2017-08-06 12:06:51 +0200

    Added basic technology stack

---
+++ src/main/kotlin/pl/humberd/googleplaybot/Google.kt
@@ -0,0 +1,16 @@
+package pl.humberd.googleplaybot
+
+import io.reactivex.Observable
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface Google {
+    @GET("/posts/1")
+    fun getPage(): Observable<MockResponse>
+
+    @POST("/posts")
+    fun postPage(@Body body: MockResponse): Observable<MockResponse>
+}
+
+data class MockResponse(val body: String)
--- src/main/kotlin/pl/humberd/googleplaybot/GooglePlayBotApplication.kt
+++ src/main/kotlin/pl/humberd/googleplaybot/GooglePlayBotApplication.kt
@@ -1,12 +1,25 @@
 package pl.humberd.googleplaybot
 
-import mu.KLogging
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 @SpringBootApplication
 class GooglePlayBotApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(GooglePlayBotApplication::class.java, *args)
+
+    val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    val service = retrofit.create(Google::class.java)
+
+    service.postPage(MockResponse("dupa"))
+            .blockingSubscribe { println(it) }
 }
